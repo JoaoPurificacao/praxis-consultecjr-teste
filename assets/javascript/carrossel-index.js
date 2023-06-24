9ee214ed53e0da1ca44e50039da780925d3b20db
Daniel Purificação <jota.daniel.38@gmail.com>
2023-06-24 11:18:49 -0300

    sessão de depoimentos

--- assets/javascript/carrossel-index.js
+++ assets/javascript/carrossel-index.js
@@ -118,3 +118,10 @@ for (area of areas){
       }
   })
 }
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
